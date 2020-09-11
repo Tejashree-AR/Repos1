e8bb62af338baec4569565ff132d2f779ec3bd93
Tejashree-AR <70144002+Tejashree-AR@users.noreply.github.com>
2020-09-11 19:48:18 +0530

    Up-Vote

---
+++ Post1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post
+{
+    class Post1
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime TimeDateCreated { get; private set; }
+        public int VoteCount { get; private set; }
+        private bool HasVotedUp;
+        private bool HasVotedDown;
+
+        public Post1(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            TimeDateCreated = DateTime.UtcNow;
+            VoteCount = 0;
+        }
+
+        public void VoteUp()
+        {
+            if (HasVotedUp)
+            {
+                throw new Exception("You have already up-voted.");
+            }
+            else
+            {
+                VoteCount++;
+                HasVotedUp = true;
+                HasVotedDown = false;
+            }
+        }
+
+        public void VoteDown()
+        {
+            if (HasVotedDown)
+            {
+                throw new Exception("You have already down-voted.");
+            }
+            else
+            {
+                VoteCount--;
+                HasVotedDown = true;
+                HasVotedUp = false;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,57 +1,25 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Project_Stack
+namespace Post
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Stack my = new Stack();
-            my.Push("--------------->Sunday"); 
-            my.Push("--------------->Monday");
-            my.Push("--------------->Tuesday");
-            my.Push("--------------->Wedenesday");
-            my.Push("--------------->Thuresday");
-            my.Push("--------------->Friday");
-            my.Push("--------------->Satuerday");
-                   
-            Console.WriteLine($"Leangth of the Stack is= { my.Count}");
-            Console.WriteLine("**************Items in Stack Are as Follows************");
-            Console.WriteLine($"Peek of the Stack is= { my.Peek()}"); //Always Give Element at the Top.
-            
-            foreach (var item in my)
-            {
-                Console.WriteLine(item);
-               
-            }
-            Console.WriteLine($"Stack Has Friday? =>{my.Contains("--------------->Friday")}");
-           Console.WriteLine($" 1st Pop Up ={my.Pop()}");
-            Console.WriteLine($" 2st Pop Up ={my.Pop()}");
-            Console.WriteLine($" 3st Pop Up ={my.Pop()}");
-
-
-
-            Console.WriteLine($"Length of the Stack after Pop ={my.Count}");
-            Console.WriteLine($"Stack Has Friday? =>{my.Contains("--------------->Friday")}");
-           // Console.WriteLine($"Stack Has Sunday=>{my.Contains("Sunday")}");
-
-
-
-           
-            my.Clear();
-            Console.WriteLine("STACK IS EMPTY");
-            Console.ReadKey();
-
-
+            Post1 post = new Post1("Does my post work?", "Test to see if my post works.");
+            System.Console.WriteLine($"Title: {post.Title}");
+            System.Console.WriteLine($"Description: {post.Description}");
+            System.Console.WriteLine($"Date Created: {post.TimeDateCreated}");
+            System.Console.WriteLine($"Post Count: {post.VoteCount}");
+            post.VoteDown();
+            System.Console.WriteLine($"Post Count: {post.VoteCount}");
+            post.VoteUp();
+            System.Console.WriteLine($"Post Count: {post.VoteCount}");
+            Console.ReadLine();
         }
-       
-
-        
     }
 }
