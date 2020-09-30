6bf31d67aeb1805c2c39da53a0ccf44e2573f454
tejashri42@gmail.com <tejashri42@gmail.com>
2020-09-30 23:43:43 +0530

    HELLO

---
+++ Project.TMS/Project.TMS.Data/TMSContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.TMS.Data
+{
+   public class TMSContext:DbContext
+
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = @"Data Source=LAPTOP-J201RQG1\SQLEXPRESS01;Catalog=Tour;Integrated Security=True";
+            optionsBuilder.UseSqlServer(connectionString);
+
+        }
+    }
+}
+ 
